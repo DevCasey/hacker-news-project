94d38bf17fb63964e759a535ad7b0f5c4232f230
Casey Calkins <c.angelcalkins@outlook.com>
2020-01-18 16:52:41 -0600

    Working on filtering the search

--- src/App.js
+++ src/App.js
@@ -7,21 +7,47 @@ class App extends Component {
     this.state = {
       newsData: [],
       titleHeader: [],
-      value: ''
+      value: '',
+      authorName: ''
     }
     this.handleSearch = this.handleSearch.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    // this.filterForInput = this.filterForInput.bind(this);
+    
   }
 
   
 
 componentDidMount() {
   this.fetchingFrontPageStories();
-  this.handleSearch();
+  // this.handleSearch();
 }
 
-handleSearch = () => {
-
+// Sets 'value' state to what is typed inside of the input field
+handleSearch(event) {
+  this.setState( {value: event.target.value} );
 }
+
+
+handleSubmit(event) {
+  event.preventDefault();
+  alert(`${this.state.value} was submitted`);
+  let findAuthor = this.state.newsData.title.find(this.state.value) 
+  this.setState({value: ''})
+  // this.filterForInput();
+}
+
+// Starts to filter what is typed in input field and searched within the 'newsData' state.
+// filterForInput() {
+//   console.log("Function Working");
+//   console.log(this.state.newsData);
+  
+// }
+
+
+
+
+
 
 
 
@@ -39,9 +65,21 @@ fetchingFrontPageStories () {
     return (
     <div className="App">
       <div className="news-data">
-        <form>
-          <input id="story-search" placeholder="Search by title or author" value={this.state.value}></input>
-          <button id="search-button" onClick={this.handleSearch}>Search</button>
+
+        <form onSubmit={this.handleSubmit}>
+
+          <input 
+          id="story-search" 
+          placeholder="Search front page stories by keywords" 
+          value={this.state.value}
+          onChange={this.handleSearch}>
+          </input>
+
+          <button id="search-button" 
+          >Search
+          </button>
+
+
         </form>
         
           {/* {this.state.newsData.map((item,index) => {
